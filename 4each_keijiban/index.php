a07df042abf2ff85dd62698f657025fa8794680e
bn777-a118@ezweb.ne.jp <bn777-a118@ezweb.ne.jp>
2019-04-16 14:30:09 +0900

    0416

--- 4each_keijiban/index.php
+++ 4each_keijiban/index.php
@@ -10,6 +10,7 @@
  </head>
     
 <body>
+   
 <header>
     <div class="logo">
         <img src="4eachblog_logo.jpg">
@@ -27,53 +28,70 @@
     <div class="left">
     
     <h1>プログラミングに役立つ掲示板</h1>
-          <h2>入力フォーム</h2>
+        
     <form method="post" action="insert.php">
+          <h2>入力フォーム</h2>
         <div>
-            <label>ハンドルネーム</label>
+            <label for="name">ハンドルネーム</label>
             <br>
-            <input size="50" type="text"name="handlename">
+            <input id="name"size="50" type="text"name="handlename">
         </div>
         <div>
-            <label>タイトル</label>
+            <label for="title">タイトル</label>
             <br>
-            <input size="50" type="text"name="title">
+            <input id="title"size="50" type="text"name="title">
         </div>
         <div>
-            <label>コメント</label>
+            <label for="comments">コメント</label>
             <br>
-            <textarea name="comments" rows="5" cols="50">
+            <textarea id="comments"name="comments" rows="5" cols="50">
             </textarea>
         </div>
         <div>
             <input type="submit"class="submit"value="投稿する">         
         </div>
-      
-        
-        
     </form>
-  
 
+          
+    
+        
+    <?php
+        
+        mb_internal_encoding("utf8");
+        $pdo = new PDO("mysql:dbname=akira;host=localhost;" ,"root" ,"mysql");
+        $stmt=$pdo->query("select * from 4each_keijiban");
+  
+     while($row=$stmt->fetch()){
+          
+        echo"<div class='kiji'>";
+        echo"<h3>".$row['title']."</h3>";
+        echo"<div class='comments'>";
+        echo $row['comments'];
+        echo"<div class='handlename'>posted by".$row['handlename']."</div>";
+        echo" </div>";
+        echo" </div>";
+     }
+        
+        
+    ?>
+        
     </div>
-        
-            
-        
-        
+   
     
     <div class="right">
-            <h3>人気の記事</h3>
+            <h4>人気の記事</h4>
                 <p>PHP オススメ本</p>
                 <p>PHP MyAdminの使い方</p>
                 <p>今人気のエディタ　Top5</p>
                 <p>HTMLの基礎</p>
 
-            <h3>オススメリンク</h3>
+            <h4>オススメリンク</h4>
                 <p>インターノウス株式会社</p>
                 <p>XAMPPのダウンロード</p>
                 <p>Eclipeのダウンロード</p>
                 <p>Bracketsのダウンロード</p>
         
-            <h3>カテゴリ</h3>
+            <h4>カテゴリ</h4>
                 <p>HTML</p>
                 <p>PHP</p>
                 <p>MySQL</p>
