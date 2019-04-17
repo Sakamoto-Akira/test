9c8cc8942be2dfaac45a5d081a8f1483e54badb3
bn777-a118@ezweb.ne.jp <bn777-a118@ezweb.ne.jp>
2019-04-17 17:47:03 +0900

    0417

---
+++ Person/src/Tset.java
@@ -0,0 +1,32 @@
+
+public class Tset {
+
+	public static void main(String[]args){
+
+		Robot aibo=new Robot();
+		aibo.name="aibo";
+
+		Robot asimo=new Robot();
+		asimo.name="asimo";
+
+		Robot pepper=new Robot();
+		pepper.name="pepper";
+
+		System.out.println("■"+aibo.name);
+		aibo.talk();
+		aibo.walk();
+		aibo.run();
+		System.out.print("\r\n");
+		System.out.println("■"+asimo.name);
+		asimo.talk();
+		asimo.walk();
+		asimo.run();
+		System.out.print("\r\n");
+		System.out.println("■"+pepper.name);
+		pepper.talk();
+		pepper.walk();
+		pepper.run();
+
+	}
+
+}
