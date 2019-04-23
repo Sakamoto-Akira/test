ae0e6930b440f9060a6580b0a33a9aa6bdc4cd16
bn777-a118@ezweb.ne.jp <bn777-a118@ezweb.ne.jp>
2019-04-23 17:55:46 +0900

    0423

--- HelloServlet/src/HelloServlet.java
+++ HelloServlet/src/HelloServlet.java
@@ -8,22 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class HelloServlet
- */
 @WebServlet("/HelloServlet")
 public class HelloServlet extends HttpServlet {
 
     public HelloServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		PrintWriter out = response.getWriter();
 		out.println("<HTML>");
 		out.println("<BODY>");
