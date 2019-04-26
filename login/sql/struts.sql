fc0674f555564725365297d60facd9035e4de491
bn777-a118@ezweb.ne.jp <bn777-a118@ezweb.ne.jp>
2019-04-26 10:12:00 +0900

    0425

--- login/sql/struts.sql
+++ login/sql/struts.sql
@@ -1,5 +1,5 @@
 set names utf8;
-set foreign_key_check = 0;
+set foreign_key_checks = 0;
 drop database if exists logindb_sakamoto;
 create database logindb_sakamoto;
 use logindb_sakamoto;
@@ -10,8 +10,8 @@ user_name varchar(255),
 password varchar(255)
 );
 
-insert into user value
+insert into user values
 (1,"taro","123"),
 (2,"jiro","123"),
 (3,"hanako","123"),
-(4,"saburo","123"),
+(4,"saburo","123");
